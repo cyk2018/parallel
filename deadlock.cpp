fe397237da1397dbefcfedd3d0404a681c5353aa
cyk2018 <57099728+cyk2018@users.noreply.github.com>
2021-06-08 15:55:52 +0800

    Update deadlock.cpp
    use a new algorithm to calculate

--- deadlock.cpp
+++ deadlock.cpp
@@ -43,58 +43,59 @@ int main(void)
 
 	//get_input(a,b,n);	//测试时用的
 	int n = sizeof(arra) / sizeof(int);
-	int jiange = n % (comm_sz - 1) ? n / (comm_sz - 1) + 1 : n / (comm_sz - 1);
+	int jiange = n - 1 / (comm_sz - 1) + 1;
 
 	//if(!my_rank) cout << "number of processes:" << comm_sz << endl;
 
-	//if (!my_rank) {	//进程0收集各个进程的结果，然后加和
-	//	cout << "n: " << n << endl;
-	//	// from no.1 process to recv data;
-	//	for (int source = 1; source < comm_sz; source++) {
-	//		MPI_Recv(&local_sum, 1, MPI_INT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	//		cout << "receive " << local_sum << " from " << source << endl;
-	//		total_sum += local_sum;
-	//	}
-	//	cout << "total_sum: " << total_sum << endl;
-	//}
-	//else
-	//{
-	//	int sum = count_sum(jiange * (my_rank - 1), MIN(jiange * my_rank - 1, n - 1));
-	//	//deadlock tag not same
-	//	//MPI_Send(&sum, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
-	//	
-	//	MPI_Send(&sum, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-	//}
-
-	//test deadlock
-	if (!my_rank)
-	{
-		//int send = 3;
-		//int recv = 4;
-		//MPI_Send(&send, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-		//MPI_Recv(&recv, 1, MPI_INT, 1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		//cout << my_rank << ": " << recv << endl;
-
-		int send[100] = { 1 };
-		int recv[100] = { 0 };
-		MPI_Send(&send, 100, MPI_INT, 1, 0, MPI_COMM_WORLD);
-		MPI_Recv(&recv, 100, MPI_INT, 1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		cout << my_rank << ": " << recv[1] << endl;
+	if (!my_rank) {	//进程0收集各个进程的结果，然后加和
+		cout << "n: " << n << endl;
+		// from no.1 process to recv data;
+		for (int source = 1; source < comm_sz; source++) {
+			MPI_Recv(&local_sum, 1, MPI_INT, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			//cout << "receive " << local_sum << " from " << source << endl;
+			total_sum += local_sum;
+		}
+		cout << "total_sum: " << total_sum << endl;
 	}
 	else
 	{
-		//int send = 5;
-		//int recv = 6;
-		//MPI_Send(&send, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
-		//MPI_Recv(&recv, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		//cout << my_rank << ": " << recv << endl;
-
-		int send[100] = { 2 };
-		int recv[100] = { 0 };
-		MPI_Send(&send, 100, MPI_INT, 0, 1, MPI_COMM_WORLD);
-		MPI_Recv(&recv, 100, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		cout << my_rank << ": " << recv[1] << endl;
+		int sum = count_sum(jiange * (my_rank - 1), MIN(jiange * my_rank - 1, n - 1));
+		//deadlock tag not same
+		//MPI_Send(&sum, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+		
+		MPI_Send(&sum, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
 	}
+
+	////test deadlock 
+	//// use "mpiexec -n 2 MPI.exe" to run 
+	//if (!my_rank)
+	//{
+	//	//int send = 3;
+	//	//int recv = 4;
+	//	//MPI_Send(&send, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+	//	//MPI_Recv(&recv, 1, MPI_INT, 1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	//	//cout << my_rank << ": " << recv << endl;
+
+	//	int send[100] = { 1 };
+	//	int recv[100] = { 0 };
+	//	MPI_Send(&send, 100, MPI_INT, 1, 0, MPI_COMM_WORLD);
+	//	MPI_Recv(&recv, 100, MPI_INT, 1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	//	cout << my_rank << ": " << recv[1] << endl;
+	//}
+	//else
+	//{
+	//	//int send = 5;
+	//	//int recv = 6;
+	//	//MPI_Send(&send, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+	//	//MPI_Recv(&recv, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	//	//cout << my_rank << ": " << recv << endl;
+
+	//	int send[100] = { 2 };
+	//	int recv[100] = { 0 };
+	//	MPI_Send(&send, 100, MPI_INT, 0, 1, MPI_COMM_WORLD);
+	//	MPI_Recv(&recv, 100, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	//	cout << my_rank << ": " << recv[1] << endl;
+	//}
 
 	MPI_Finalize();
 	return 0;
